579de0190076007dfd4bbe305522a668e4320695
ashutosh <ashu.p89@gmail.com>
2021-07-13 12:41:59 +0530

    initial code base

---
+++ src/main/java/com/ashuz/ashuzimagehoster/AshuzimagehosterApplication.java
@@ -0,0 +1,13 @@
+package com.ashuz.ashuzimagehoster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshuzimagehosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshuzimagehosterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashuz/ashuzimagehoster/AshuzimagehosterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashuz.ashuzimagehoster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AshuzimagehosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
